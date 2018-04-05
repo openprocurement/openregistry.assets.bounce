039136a7ebe24297e2e7b956271841041fce27c9
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-04-05 19:08:07 +0300

    Add roles

---
+++ openregistry/assets/loki/roles.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from schematics.transforms import whitelist, blacklist
+from openprocurement.api.models.registry_models.roles import schematics_embedded_role
+
+plain_role = (blacklist('_attachments', 'revisions', 'dateModified', 'rectificationPeriod') + schematics_embedded_role)
+
+create_role = (blacklist('owner_token', 'owner', '_attachments', 'revisions', 'date', 'dateModified', 'doc_id', 'assetID', 'documents', 'status', 'rectificationPeriod') + schematics_embedded_role)
+edit_role = (blacklist('assetType', 'owner_token', 'owner', '_attachments', 'revisions', 'date', 'dateModified', 'doc_id', 'assetID', 'documents', 'mode', 'rectificationPeriod') + schematics_embedded_role)
+view_role = (blacklist('owner_token', '_attachments', 'revisions') + schematics_embedded_role)
+
+Administrator_role = whitelist('status', 'mode', 'relatedLot')
+concierge_role = (whitelist('status', 'relatedLot'))
