147826883b1ce4cf8be27c8733ac4261cf1451ce
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-04-06 16:27:27 +0300

    Update roles

--- openregistry/assets/loki/roles.py
+++ openregistry/assets/loki/roles.py
@@ -4,8 +4,8 @@ from openprocurement.api.models.registry_models.roles import schematics_embedded
 
 plain_role = (blacklist('_attachments', 'revisions', 'dateModified', 'rectificationPeriod') + schematics_embedded_role)
 
-create_role = (blacklist('owner_token', 'owner', '_attachments', 'revisions', 'date', 'dateModified', 'doc_id', 'assetID', 'documents', 'status', 'rectificationPeriod') + schematics_embedded_role)
-edit_role = (blacklist('assetType', 'owner_token', 'owner', '_attachments', 'revisions', 'date', 'dateModified', 'doc_id', 'assetID', 'documents', 'mode', 'rectificationPeriod') + schematics_embedded_role)
+create_role = (blacklist('owner_token', 'owner', '_attachments', 'revisions', 'date', 'dateModified', 'doc_id', 'assetID', 'documents', 'status', 'rectificationPeriod', 'items') + schematics_embedded_role)
+edit_role = (blacklist('assetType', 'owner_token', 'owner', '_attachments', 'revisions', 'date', 'dateModified', 'doc_id', 'assetID', 'documents', 'mode', 'rectificationPeriod', 'items') + schematics_embedded_role)
 view_role = (blacklist('owner_token', '_attachments', 'revisions') + schematics_embedded_role)
 
 Administrator_role = whitelist('status', 'mode', 'relatedLot')
