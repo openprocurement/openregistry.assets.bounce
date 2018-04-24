43692d8380bfd048aa9c764618ffcc6286276aba
Andrew Leitsius <leits@quintagroup.com>
2018-04-24 16:24:20 +0300

    Add logging in inludeme

--- openregistry/assets/bounce/includeme.py
+++ openregistry/assets/bounce/includeme.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+import logging
+
 from pyramid.interfaces import IRequest
 from openregistry.assets.core.interfaces import IContentConfigurator, IAssetManager
 from openregistry.assets.bounce.models import Asset, IBounceAsset
 from openregistry.assets.bounce.adapters import BounceAssetConfigurator, BounceAssetManagerAdapter
+
+LOGGER = logging.getLogger(__name__)
 
 
 def includeme(config, plugin_config=None):
@@ -15,3 +19,5 @@ def includeme(config, plugin_config=None):
     config.registry.registerAdapter(BounceAssetManagerAdapter,
                                     (IBounceAsset,),
                                     IAssetManager)
+
+    LOGGER.info("Included openregistry.lots.loki plugin", extra={'MESSAGE_ID': 'included_plugin'})
