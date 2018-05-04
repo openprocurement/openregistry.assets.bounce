e274fc62356cf82efd84d6d1228072c50ccc50c5
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-05-04 09:43:38 +0300

    Move asset.py logic to core

--- openregistry/assets/bounce/adapters.py
+++ openregistry/assets/bounce/adapters.py
@@ -2,6 +2,8 @@
 from openregistry.assets.core.adapters import AssetConfigurator, AssetManagerAdapter
 from openregistry.assets.core.constants import STATUS_CHANGES
 
+from openregistry.assets.bounce.validation import validate_deleted_status
+
 
 class BounceAssetConfigurator(AssetConfigurator):
     """ BelowThreshold Tender configuration adapter """
@@ -14,9 +16,13 @@ class BounceAssetManagerAdapter(AssetManagerAdapter):
     name = "Asset Manager for bounce asset"
     context = None
     create_validation = []
+    change_validation = [validate_deleted_status]
 
     def __init__(self, context):
         self.context = context
 
     def create_asset(self, request):
         self._validate(request, self.create_validation)
+
+    def change_asset(self, request):
+        self._validate(request, self.change_validation)
