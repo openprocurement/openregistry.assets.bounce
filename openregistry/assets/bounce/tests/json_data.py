0844a5d0c8cb58d2a0cdb4288ef409aa01fd3177
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-05-17 19:09:39 +0300

    Add yoku aliase to app_meta.yaml. Set type from env var for tests

--- openregistry/assets/bounce/tests/json_data.py
+++ openregistry/assets/bounce/tests/json_data.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
+
 from copy import deepcopy
 from openregistry.assets.core.tests.blanks.json_data import (
     test_organization,
@@ -12,11 +14,11 @@ from openregistry.assets.core.utils import (
 test_item_data = deepcopy(test_loki_item_data)
 
 # test_loki_item_data['schema_properties'] = schema_properties
-
+asset_type = os.environ.get('ASSET_TYPE', 'bounce')
 test_asset_bounce_data = {
     "title": u"Земля для космодрому",
     "description": u"Опис землі для космодрому",
-    "assetType": "bounce",
+    "assetType": asset_type,
     "items": [test_item_data, test_item_data],
     "assetCustodian": deepcopy(test_organization),
     "decisions": [{
