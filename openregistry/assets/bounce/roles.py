2784ca2ca154ab1dceb9a24fec1ee54bb40a835f
Andrew Leitsius <leits@quintagroup.com>
2018-06-05 18:42:41 +0300

    Blacklisted transfer_token

--- openregistry/assets/bounce/roles.py
+++ openregistry/assets/bounce/roles.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from schematics.transforms import whitelist, blacklist
-from openregistry.assets.core.models import schematics_embedded_role, listing_role, schematics_default_role
+from openregistry.assets.core.models import assets_embedded_role, listing_role, schematics_default_role
 
-plain_role = (blacklist('_attachments', 'revisions', 'dateModified', 'rectificationPeriod') + schematics_embedded_role)
+plain_role = (blacklist('_attachments', 'revisions', 'dateModified', 'rectificationPeriod') + assets_embedded_role)
 
-create_role = (blacklist('owner_token', 'owner', '_attachments', 'revisions', 'date', 'dateModified', 'doc_id', 'assetID', 'documents', 'status', 'rectificationPeriod') + schematics_embedded_role)
-edit_role = (blacklist('assetType', 'owner_token', 'owner', '_attachments', 'revisions', 'date', 'dateModified', 'doc_id', 'assetID', 'documents', 'mode', 'rectificationPeriod', 'items') + schematics_embedded_role)
-view_role = (blacklist('owner_token', '_attachments', 'revisions') + schematics_embedded_role)
+create_role = (blacklist('owner_token', 'owner', '_attachments', 'revisions', 'date', 'dateModified', 'doc_id', 'assetID', 'documents', 'status', 'rectificationPeriod') + assets_embedded_role)
+edit_role = (blacklist('assetType', 'owner_token', 'owner', '_attachments', 'revisions', 'date', 'dateModified', 'doc_id', 'assetID', 'documents', 'mode', 'rectificationPeriod', 'items') + assets_embedded_role)
+view_role = (blacklist('owner_token', '_attachments', 'revisions') + assets_embedded_role)
 
 Administrator_role = whitelist('status', 'mode', 'relatedLot')
 concierge_role = (whitelist('status', 'relatedLot'))
