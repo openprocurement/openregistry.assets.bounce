6bada4df8173e51c5360a041360d939bb211434e
vladkhard <vladkhard@gmail.com>
2018-06-19 17:46:42 +0300

    add onwer transfer support

--- openregistry/assets/bounce/tests/base.py
+++ openregistry/assets/bounce/tests/base.py
@@ -5,7 +5,8 @@ from copy import deepcopy
 from openregistry.assets.core.tests.base import (
     BaseAssetWebTest as BaseAWT, #snitch
     connection_mock_config,
-    MOCK_CONFIG as BASE_MOCK_CONFIG
+    MOCK_CONFIG as BASE_MOCK_CONFIG,
+    AssetTransferWebTest as AssetTWT
 )
 from json_data import test_asset_bounce_data
 from openregistry.assets.bounce.tests.fixtures import PARTIAL_MOCK_CONFIG
@@ -25,6 +26,16 @@ class BaseAssetWebTest(BaseAWT):
     def setUp(self):
         self.initial_data = deepcopy(test_asset_bounce_data)
         super(BaseAssetWebTest, self).setUp()
+
+
+class AssetTransferWebTest(AssetTWT):
+    initial_auth = ('Basic', ('broker', ''))
+    relative_to = os.path.dirname(__file__)
+    mock_config = MOCK_CONFIG
+
+    def setUp(self):
+        self.initial_data = deepcopy(test_asset_bounce_data)
+        super(AssetTransferWebTest, self).setUp()
 
 
 class AssetContentWebTest(BaseAssetWebTest):
