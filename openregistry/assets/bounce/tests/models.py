cfe13dbb967a579702af97da0b2689be4b97212b
MichaelYusko <mykhaylo.yusko@gmail.com>
2018-06-25 14:49:29 +0300

    Add Test class for the sandboxParameters

---
+++ openregistry/assets/bounce/tests/models.py
@@ -0,0 +1,12 @@
+import unittest
+
+from openregistry.assets.core.constants import SANDBOX_MODE
+from openregistry.assets.bounce.models import Asset
+
+
+class TestSandBoxParameters(unittest.TestCase):
+
+    @unittest.skipIf(not SANDBOX_MODE, 'If sandbox mode was\'t present')
+    def test_parameter_is_present(self):
+        sandbox_present = hasattr(Asset, 'sandboxParameters')
+        self.assertEqual(True, sandbox_present)
