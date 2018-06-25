83077c73744186398cd50ecccffd0151d740bd2f
MichaelYusko <mykhaylo.yusko@gmail.com>
2018-06-25 16:19:37 +0300

    Change hasattr function on Asset instance

--- openregistry/assets/bounce/tests/models.py
+++ openregistry/assets/bounce/tests/models.py
@@ -8,5 +8,5 @@ class TestSandBoxParameters(unittest.TestCase):
 
     @unittest.skipIf(not SANDBOX_MODE, 'If sandbox mode was\'t present')
     def test_parameter_is_present(self):
-        sandbox_present = hasattr(Asset, 'sandboxParameters')
+        sandbox_present = 'sandboxParameters' in Asset()._fields
         self.assertEqual(True, sandbox_present)
