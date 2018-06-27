2b9644b57d685de74b03d78d0f6c29718f86a901
kenkaneki <mykhaylo.yusko@gmail.com>
2018-06-27 16:07:34 +0300

    Add sandbox_parameter (#37)
    * Add sandbox_parameter
    
    * little refactor
    
    * Add Test class for the sandboxParameters
    
    * Change hasattr function on Asset instance
    
    * Rewrite constant import

--- openregistry/assets/bounce/tests/json_data.py
+++ openregistry/assets/bounce/tests/json_data.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 import os
-
 from copy import deepcopy
+
+from openregistry.assets.core.constants import SANDBOX_MODE
+from openregistry.assets.core.tests.base import DEFAULT_ACCELERATION
 from openregistry.assets.core.tests.blanks.json_data import (
     test_organization_loki,
     schema_properties,
     test_loki_item_data
 )
-from openregistry.assets.core.utils import (
-    get_now
-)
+from openregistry.assets.core.utils import get_now
 
 test_item_data = deepcopy(test_loki_item_data)
 del test_item_data['id']
@@ -27,5 +27,8 @@ test_asset_bounce_data = {
         'decisionID': '1111-4'
     }]
 }
+
+if SANDBOX_MODE:
+    test_asset_bounce_data['sandboxParameters'] = 'quick, accelerator={}'.format(DEFAULT_ACCELERATION)
 
 
