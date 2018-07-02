e4b6a5b8581da09c356ab7df105c98556f0f9fd4
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-07-02 13:32:49 +0300

    Update roles and permissions

--- openregistry/assets/bounce/roles.py
+++ openregistry/assets/bounce/roles.py
@@ -4,18 +4,30 @@ from openregistry.assets.core.models import assets_embedded_role, listing_role, 
 
 plain_role = (blacklist('_attachments', 'revisions', 'dateModified', 'rectificationPeriod') + assets_embedded_role)
 
-create_role = (blacklist('owner_token', 'owner', '_attachments', 'revisions', 'date', 'dateModified', 'doc_id', 'assetID', 'documents', 'status', 'rectificationPeriod') + assets_embedded_role)
-edit_role = (blacklist('assetType', 'owner_token', 'owner', '_attachments', 'revisions', 'date', 'dateModified', 'doc_id', 'assetID', 'documents', 'mode', 'rectificationPeriod') + assets_embedded_role)
+create_role = (
+    blacklist(
+        'owner_token', 'owner', '_attachments', 'revisions', 'date', 'decisions',
+        'dateModified', 'doc_id', 'assetID', 'documents', 'status', 'rectificationPeriod'
+    ) +
+    assets_embedded_role
+)
+edit_role = (
+    blacklist(
+        'assetType', 'owner_token', 'owner', '_attachments', 'revisions', 'date', 'decisions',
+        'dateModified', 'doc_id', 'assetID', 'documents', 'mode', 'rectificationPeriod') +
+    assets_embedded_role
+)
 view_role = (blacklist('owner_token', '_attachments', 'revisions') + assets_embedded_role)
 
 Administrator_role = whitelist('status', 'mode', 'relatedLot')
 concierge_role = (whitelist('status', 'relatedLot'))
 
 decision_roles = {
+    'view': (schematics_default_role + blacklist()),
     'create': blacklist('decisionOf', 'relatedItem'),
     'edit': blacklist('decisionOf', 'relatedItem'),
-    'edit_draft': blacklist('decisionOf', 'relatedItem'),
-    'edit_pending': blacklist('decisionOf', 'relatedItem'),
+    'not_edit': whitelist()
+
 }
 
 asset_roles = {
