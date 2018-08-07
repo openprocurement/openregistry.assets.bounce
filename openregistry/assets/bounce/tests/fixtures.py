402474d748af33ebc3ea335b7e11055080831122
vitalii-motso <vitalii.motso@gmail.com>
2018-08-07 15:35:52 +0300

    rm level of accreditation from models

--- openregistry/assets/bounce/tests/fixtures.py
+++ openregistry/assets/bounce/tests/fixtures.py
@@ -1,6 +1,10 @@
 PARTIAL_MOCK_CONFIG = {
     "assets.bounce":{
         "use_default":True,
-        "aliases":["domain"]
+        "aliases":["domain"],
+        "accreditation": {
+            "create": [3],
+            "edit": [4]
+        }
     }
 }
